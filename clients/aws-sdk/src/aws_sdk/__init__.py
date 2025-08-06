1124e871fc3c2a0df08d3b2d264261188091e246
jonathan343 <jonathangee09@gmail.com>
2025-08-06 13:45:55 -0400

    Add version attribute to aws-sdk

--- clients/aws-sdk/src/aws_sdk/__init__.py
+++ clients/aws-sdk/src/aws_sdk/__init__.py
@@ -1,4 +1,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+import importlib.metadata
+
+__version__: str = importlib.metadata.version(__name__)
+
 
 # TODO: Consider adding relative imports for services from the top level namespace?
