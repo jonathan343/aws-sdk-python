2d8f9746d1fa5add34c8d46e717a45e70b62d90f
SamRemis <sjremis94@gmail.com>
2025-11-21 19:53:04 -0500

    Release AWS SDK Python 0.2.0 (#34)

--- clients/aws-sdk-python/src/aws_sdk_python/__init__.py
+++ clients/aws-sdk-python/src/aws_sdk_python/__init__.py
@@ -1,6 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 
 # TODO: Consider adding relative imports for services from the top level namespace?
