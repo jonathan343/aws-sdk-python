3c94529e548dc30cfa4a11a2d532edf6bbb288b8
Nate Prewitt <nate.prewitt@gmail.com>
2026-01-02 09:54:59 -0700

    Release: AWS SDK Transcribe Streaming 0.3.0

--- clients/aws-sdk-transcribe-streaming/docs/conf.py
+++ clients/aws-sdk-transcribe-streaming/docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 project = "Amazon Transcribe Streaming Service"
 author = "Amazon Web Services"
-release = "0.2.0"
+release = "0.3.0"
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
